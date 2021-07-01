669c1cafc901bde8156fc97f736647dad30b1316
kailog <publicemailforgit@gmail.com>
2021-07-01 19:41:31 +0900

    feat: ユーザーページでタスクをaxios経由で取得するよう変更した。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @tasks = { url: 'https://atcoder.jp/contests/zone2021/tasks/zone2021_a', url: 'https://atcoder.jp/contests/zone2021/tasks/zone2021_b'}
+    render json: {tasks: [
+                 {"id": 1, "url": "https://kai2020.blog"},
+                 {"id": 2, "url": "https://kai2020.blog"},
+                 {"id": 3, "url": "https://kai2020.blog"},
+    ]}
   end
 
   def show
