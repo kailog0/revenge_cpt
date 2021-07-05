40083ecd913adcab5ce55abb5eb7b60fb5daccbf
kailog <publicemailforgit@gmail.com>
2021-07-05 22:48:09 +0900

    feat: タスク登録用API作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  protect_from_forgery except: :create
   def index
     render json: {tasks: [
                  {"id": 1, "url": "https://kai2020.blog"},
@@ -10,5 +11,14 @@ class TasksController < ApplicationController
   def show
     tasks = { url: 'https://atcoder.jp/contests/zone2021/tasks/zone2021_a', url: 'https://atcoder.jp/contests/zone2021/tasks/zone2021_b'}
     render json: { status: 'SUCCESS', data: tasks}
+  end
+
+  def create
+    task = Task.new(url: params[:url], user_id: current_user.id, status: 0)
+    if task.save
+      render json: {'message': '登録完了'}, status: 200
+    else
+      head :bad_request
+    end
   end
 end
