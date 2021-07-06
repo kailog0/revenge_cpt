f1faaf8f946e2c95c9988eecf33c6ec8f421fdfb
kailog <publicemailforgit@gmail.com>
2021-07-06 15:43:45 +0900

    feat: タスク登録件数の上限を設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
   protect_from_forgery except: :create
+
+  TASK_UPPER_LIMIT = 5000;
+
   def index
     render json: {tasks: [
                  {"id": 1, "url": "https://kai2020.blog"},
@@ -15,7 +18,13 @@ class TasksController < ApplicationController
 
   def create
     task = Task.new(url: params[:url], user_id: current_user.id, status: 0)
-    if task.save
+    tasks_count = Task.where(user_id: current_user).count
+    error_messages = []
+    if TASK_UPPER_LIMIT >= tasks_count 
+      error_messages.push(ErrorHelper::TASK_UPPER_LIMIT_ERROR_MESSAGE)
+    end
+
+    if TASK_UPPER_LIMIT > tasks_count && task.save
       render json: {'message': '登録完了'}, status: 200
     else
       head :bad_request
