9fb1ebca0a170be48d6e97f44fe63c1dd6b805c0
kailog <publicemailforgit@gmail.com>
2021-07-14 13:19:22 +0900

    fix: vueインスタンスを代入する処理を削除

--- app/javascript/packs/user_vue.js
+++ app/javascript/packs/user_vue.js
@@ -3,7 +3,7 @@ import TableComponent from '../src/table_component.vue'
 import ConfirmUrlComponent from '../src/confirm_url.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
+  new Vue({
     el: '#app',
     components: { 
         TableComponent,
